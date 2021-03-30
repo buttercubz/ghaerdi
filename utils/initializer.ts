9a3643b580c9094fb41a8c655796c1556f1489a6
ghaerdi <ghaerdi@ipisdosaj.edu.do>
2021-03-30 19:18:03 -0400

    adding test for newSocialLink function

--- utils/initializer.ts
+++ utils/initializer.ts
@@ -1,5 +1,11 @@
 import { SocialLink } from "./types";
 
+/**
+ * @param {string} title 
+ * @param {string} [url]
+ * @returns {SocialLink}
+ */
+
 export function newSocialLink(title: string, url?: string): SocialLink {
   const lowerCaseTitle = title.toLowerCase();
   const path = `/icons/${lowerCaseTitle}.svg`;
