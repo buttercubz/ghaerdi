d747fd4e1dad720dea83571f0df0363c534eb623
ghaerdi <ghaerdi@ipisdosaj.edu.do>
2021-08-18 21:11:40 -0400

    remove hello world msg

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,111 @@
 <template>
-  <div>
-    <polygonBackground/>
-    <div class="container d-flex center">
-      <h3>Hello World</h3>
+  <div class="overflow-hidden">
+    <polygonBackground />
+
+    <div class="comming-from-left-animation container d-flex align-items-center">
+      <div class="about d-flex column">
+        <span class="header">
+          <h1 class="bold">Gil Rudolf Härdi (ghaerdi)</h1>.
+        </span>
+        <br />
+
+        <span class="transparent">
+          <div class="d-inline-flex">
+            I'm a
+            <ul class="dynamic-text">
+              <li><h1>software developer.</h1></li>
+              <li><h1>fullstack developer.</h1></li>
+              <li><h1>web developer.</h1></li>
+            </ul>
+          </div>
+          <br>
+
+          I am in love with <h2>TypeScript</h2>. I also use technologies
+          like <h2>React</h2> and <h2>Vue</h2>.
+          <br />
+          <br />
+
+          Besides programming I like to spend time watching anime, playing video
+          games and drawing.
+        </span>
+        <socialLinks class="visible-animation" />
+      </div>
     </div>
+
+    <div class="comming-from-right-animation">
+      <NuxtLink class="btn p-absolute flashing-animation bottom right" to="/projects">
+        Click here to see my projects
+      </NuxtLink >
+    </div>
+
   </div>
 </template>
 
-<script>
-  export default {
-    created() {
-      setTimeout(() => this.$router.push({ path: '/me'}), 2500);
-    },
-  };
-</script>
-
 <style scoped>
-  .container {
-    max-width: 100vw;
-    max-height: 100vh;
+  .dynamic-text {
+    margin: 0;
+    padding: 0;
+    margin-left: 0.75rem;
+    text-align: left !important;
+    height: 2rem;
+    overflow: hidden;
   }
 
-  h3 {
-    opacity: 0;
-    padding: 10px;
-    text-transform: uppercase;
-    letter-spacing: 20px;
-    animation: inAndOut 2500ms ease-in;
+  .dynamic-text li {
+    list-style: none;
+    position: relative;
+    top: 0;
+    animation: slide 6s steps(3) infinite;
+    animation-delay: .5s;
   }
 
-  @keyframes inAndOut {
-    0% {transform: scale(0) translate(0, -500%); opacity: 0;}
-    30% {transform: scale(1) translate(0, 0); opacity: var(--text-opacity);}
-    90% {transform: scale(1) translate(0, 0); opacity: var(--text-opacity);}
-    100% {transform: scale(0.5) translate(0, 500%); opacity: 0;}
+  @keyframes slide {
+    100% {
+      top: -6rem;
+    }
+  }
+
+  .dinamic-text li h1 {
+    position: relative;
+    margin: 15px 0;
+  }
+
+  .dynamic-text li h1::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    border-left: solid 2px var(--white-color);
+    background-color: var(--secondary-background-color);
+    animation: typing 2s steps(20) infinite;
+    animation-delay: .5s;
+  }
+
+  @keyframes typing {
+    30%, 70% {
+      left: 100%;
+    }
+  }
+
+  .about {
+    width: 30vw;
+    min-width: 550px;
+    margin-left: 10vw;
+  }
+
+  @media screen and (max-width: 800px) {
+    .about {
+      margin: 0;
+      min-width: 0;
+      width: 80vw;
+    }
+  }
+
+  @media screen and (max-width: 650px) {
+    .about {
+      width: 95vw;
+      text-align: center;
+    }
   }
 </style>
